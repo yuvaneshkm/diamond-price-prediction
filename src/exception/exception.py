c090a23d529404f8a9b4acb6a7d48ae5ca8e4866
yuvaneshkm <yuvaneshkm05@gmail.com>
2024-07-08 23:12:57 +0530

    'exception-file'

--- src/exception/exception.py
+++ src/exception/exception.py
@@ -2,38 +2,26 @@
 import sys
 
 
-# function for custom exception message:
-def error_message_detail(msg, error_detail) -> str:
-    _, _, exc_tb = error_detail.exc_info()
-    # file with error:
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    # line where error occured
-    line_number = exc_tb.tb_lineno
-    # type of error
-    message = str(msg)
-    # Error Message:
-    error_message = (
-        "Error occured in Python script name: [{}], "
-        "line number: [{}] and error message: [{}]"
-    ).format(file_name, line_number, message)
-    return error_message
-
-
 # Creating a class to handle exceprion by inheriting Exception class:
 class CustomException(Exception):
-    # constructor:
-    def __init__(self, msg, error_detail):
 
-        super().__init__(msg)
-        self.error_message = error_message_detail(msg, error_detail)
+    def __init__(self, msg):
+        _, _, exc_tb = sys.exc_info()
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_number = exc_tb.tb_lineno
+        message = str(msg)
+        self.error_message = (
+            f"Error occured in Python script name: [{file_name}], "
+            f"line number: [{line_number}] and error message: [{message}]"
+        )
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.error_message
 
 
 if __name__ == "__main__":
     try:
         print("Hello")
-        9 / "k"
+        9 / 0
     except Exception as e:
-        print(CustomException(e, sys))
+        print(CustomException(e))
