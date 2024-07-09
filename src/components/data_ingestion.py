067c2f820037e36a9321c8fe2a90f7b78c19e51a
yuvaneshkm <yuvaneshkm05@gmail.com>
2024-07-10 01:06:00 +0530

    'data-ingested'

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,29 +1,91 @@
 # Importing necessary libraries:
 import os
-import sys
-from pathlib import Path
 from dataclasses import dataclass
 from src.logger import logging
 from src.exception import CustomException
-import pandas as pd
-import numpy as np
+from mdb_connect_pkg import mongo_crud
 from sklearn.model_selection import train_test_split
 import warnings
-warnings.filterwarnings('ignore')
+
+warnings.filterwarnings("ignore")
 
 
 # Inputs related to data_ingestion component:
 @dataclass
 class DataIngestionConfig:
-    pass
+    raw_data_path = os.path.join("artifacts", "raw.csv")
+    train_data_path = os.path.join("artifacts", "train.csv")
+    test_data_path = os.path.join("artifacts", "test.csv")
+
 
 class DataIngestion:
 
     def __init__(self):
-        pass
+        self.ingestion_config = DataIngestionConfig()
 
     def initiate_data_ingestion(self):
+        logging.info("Data Ingestion Started")
         try:
-            pass
+            # Initializing mongodb connector:
+            logging.info("Initializing mongodb connector")
+            URL = "mongodb://localhost:27017"
+            DATABASE_NAME = "projectdb"
+            COLLECTION_NAME = "diamond_price_data"
+            mongo = mongo_crud.MongoDBConnection(URL, DATABASE_NAME, COLLECTION_NAME)
+
+            # Creating mongodb client:
+            logging.info("Creating mongodb client")
+            mongo.create_mongo_client()
+            mongo.database_()
+            mongo.collection_()
+
+            # Loading data from mongodb:
+            logging.info("Loading data from mongodb")
+            df = mongo.load_data()
+            df.drop("id", axis=1, inplace=True)
+
+            # creating artifact directory:
+            parent_dir = os.path.abspath(os.path.join(os.getcwd(), "../.."))
+            artifact_dir = os.path.join(
+                parent_dir, os.path.dirname(self.ingestion_config.raw_data_path)
+            )
+            os.makedirs(artifact_dir, exist_ok=True)
+
+            # saving raw data in artifacts folder:
+            raw_df_path = os.path.join(parent_dir, self.ingestion_config.raw_data_path)
+            df.to_csv(raw_df_path, index=False)
+            logging.info("Saved raw data in artifact folder")
+
+            # train test split:
+            logging.info("Performing train test split")
+            train_df, test_df = train_test_split(df, test_size=0.3, random_state=21)
+
+            # saving train data in artifacts folder:
+            train_df_path = os.path.join(
+                parent_dir, self.ingestion_config.train_data_path
+            )
+            train_df.to_csv(train_df_path, index=False)
+            logging.info("Saved train data in artifact folder")
+
+            # saving test data in artifacts folder:
+            test_df_path = os.path.join(
+                parent_dir, self.ingestion_config.test_data_path
+            )
+            test_df.to_csv(test_df_path, index=False)
+            logging.info("Saved test data in artifact folder")
+
+            logging.info("Data Ingestion Completed")
+
+            return (
+                self.ingestion_config.raw_data_path,
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path,
+            )
+
         except Exception as ex:
             logging.info(CustomException(ex))
+
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    print(obj.initiate_data_ingestion())
