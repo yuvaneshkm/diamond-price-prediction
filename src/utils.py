2c5467fccec85694b67665a180954ef839d47a38
yuvaneshkm <yuvaneshkm05@gmail.com>
2024-07-10 10:58:20 +0530

    'load-object-function'

--- src/utils.py
+++ src/utils.py
@@ -9,17 +9,25 @@ import numpy as np
 import pickle
 
 
-
 # saving whether a model or a preprocessing object:
 def save_object(directory:Path, filename:str, object):
     try:
         directory = Path(directory)
         os.makedirs(directory, exist_ok=True)
         filepath = os.path.join(directory, filename)
-        with open(filepath, 'wb') as f:
-            pickle.dump(object, f)
-        f.close()
+        with open(filepath, 'wb') as file_obj:
+            pickle.dump(object, file_obj)
+        file_obj.close()
     except Exception as ex:
         logging.info(CustomException(ex))
 
+
 # loading the model or preprocessing object:
+def load_object(filepath:Path):
+    try:
+        filepath = Path(filepath)
+        with open(filepath) as file_obj:
+            return pickle.load(file_obj)
+        file_obj.close()
+    except Exception as ex:
+        logging.info(CustomException(ex))
