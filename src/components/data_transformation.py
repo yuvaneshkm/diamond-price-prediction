c25347c8d0d8c219e1939103da9598f3219bb741
yuvaneshkm <yuvaneshkm05@gmail.com>
2024-07-11 08:43:37 +0530

    'data-transformation'

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,6 +1,6 @@
 # Importing necessary libraries:
 import os
-import sys
+from pathlib import Path
 from src.logger import logging
 from src.exception import CustomException
 from dataclasses import dataclass
@@ -9,40 +9,75 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import RobustScaler, OrdinalEncoder
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
-import pickle
-from src.utils import numeric_categoric_columns
+from src.utils import numeric_categoric_columns, categoric_col_order, save_object
 from src.components import data_ingestion
 import warnings
-warnings.filterwarnings('ignore')
+
+warnings.filterwarnings("ignore")
 
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path = os.path.join("artifacts", "preprocessor.pkl")
+    preprocessor_obj_file_name = "preprocessor.pkl"
 
 
 class DataTransformation:
 
-    # Constructor:
     def __init__(self):
-        pass
-
+        self.data_transformation_config = DataTransformationConfig()
 
     # Get Data Transformation:
-    def get_data_transformation(self, raw_data_path):
+    def get_data_transformer(self, raw_data_path: Path):
         try:
             # data transformation:
             logging.info("Data Preprocessing initiated")
-            num_col, cate_col = numeric_categoric_columns(raw_data_path)
 
+            # get numeric and categoric columns:
+            logging.info("Getting Numeric and Categoric columns")
+            num_cols, cate_cols = numeric_categoric_columns(raw_data_path)
+            logging.info("Catrgorical columns order")
+            cut_order, color_order, clarity_order = categoric_col_order()
 
-            pass
+            # Preprocessor Pipeline construction:
+            logging.info("Constructing Preprocessor Pipeline")
+
+            # Numeric Pipeline:
+            num_step = [
+                ("impute_missing", SimpleImputer(strategy="median")),
+                ("feature_scaling", RobustScaler()),
+            ]
+            numeric_pipeline = Pipeline(num_step)
+            logging.info("Numeric Pipeline Constructed")
+
+            # Categoric Pipeline:
+            cate_step = [
+                ("impute_missing", SimpleImputer(strategy="most_frequent")),
+                (
+                    "encoding",
+                    OrdinalEncoder(categories=[cut_order, color_order, clarity_order]),
+                ),
+            ]
+            categoric_pipeline = Pipeline(cate_step)
+            logging.info("Categoric Pipeline Constructed")
+
+            # Final Preprocessor Pipeline:
+            final_pre_step = [
+                ("num_pre", numeric_pipeline, num_cols),
+                ("cate_pre", categoric_pipeline, cate_cols),
+            ]
+            preprocessor = ColumnTransformer(final_pre_step, remainder="passthrough")
+            preprocessor.set_output(transform="pandas")
+            logging.info("Final Preprocessor Pipeline Constructed")
+
+            return preprocessor
+
         except Exception as ex:
             logging.info(CustomException(ex))
 
-
     # Initiate Data Transformation:
-    def initiate_data_transformation(self,raw_data_path, train_data_path, test_data_path):
+    def initiate_data_transformation(
+        self, raw_data_path: Path, train_data_path: Path, test_data_path: Path
+    ):
         try:
             # loading train and test data:
             logging.info("Loading train and test data")
@@ -52,19 +87,52 @@ class DataTransformation:
             logging.info(f"Test DataFrame: \n {test_df.head()}")
 
             # get preprocessing object:
-            preprocessing_obj = self.get_data_transformation(raw_data_path)
+            logging.info("Loading Preprocessor object")
+            preprocessor_obj = self.get_data_transformer(raw_data_path)
 
+            # Train data --> Dependent and Independent features:
+            logging.info("Split Dependent and Independent variable of Train Data")
+            X_train_df = train_df.drop("price", axis=1)
+            y_train_df = train_df[["price"]]
 
+            # Test data --> Dependent and Independent features:
+            logging.info("Split Dependent and Independent variable of Test Data")
+            X_test_df = test_df.drop("price", axis=1)
+            y_test_df = test_df[["price"]]
 
+            # preprocessing on test and train data:
+            logging.info("Preprocess Train and Test data")
+            pre_X_train_df = preprocessor_obj.fit_transform(X_train_df)
+            logging.info(f"Preprocessed Train data \n {pre_X_train_df.head()}")
+            pre_X_test_df = preprocessor_obj.transform(X_test_df)
+            logging.info(f"Preprocessed Test data \n {pre_X_test_df.head()}")
 
-            pass
+            # Final Preprocessed Train and Test data:
+            pre_train_df = pd.concat([pre_X_train_df, y_train_df], axis=1)
+            pre_test_df = pd.concat([pre_X_test_df, y_test_df], axis=1)
+
+            # save the preprocessor object in artifact folder:
+            directory = Path(os.path.dirname(raw_data_path))
+            save_object(
+                directory,
+                self.data_transformation_config.preprocessor_obj_file_name,
+                preprocessor_obj,
+            )
+            logging.info("Saved the Preprocessor Object at artifacts folder")
+
+            return (pre_train_df, pre_test_df)
+
         except Exception as ex:
             logging.info(CustomException(ex))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     diobj = data_ingestion.DataIngestion()
-    raw_data_path, train_data_path, test_data_path, = diobj.initiate_data_ingestion()
+    (
+        raw_data_path,
+        train_data_path,
+        test_data_path,
+    ) = diobj.initiate_data_ingestion()
 
     obj = DataTransformation()
     obj.initiate_data_transformation(raw_data_path, train_data_path, test_data_path)
--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,20 @@
 # Importing necessary libraries:
 import os
-import sys
 from pathlib import Path
+from typing import List, Tuple
 from src.logger import logging
 from src.exception import CustomException
 import pandas as pd
-import numpy as np
 import pickle
 
 
 # saving whether a model or a preprocessing object:
-def save_object(directory:Path, filename:str, object):
+def save_object(directory: Path, filename: str, object):
     try:
         directory = Path(directory)
         os.makedirs(directory, exist_ok=True)
         filepath = os.path.join(directory, filename)
-        with open(filepath, 'wb') as file_obj:
+        with open(filepath, "wb") as file_obj:
             pickle.dump(object, file_obj)
         file_obj.close()
     except Exception as ex:
@@ -23,10 +22,10 @@ def save_object(directory:Path, filename:str, object):
 
 
 # loading the model or preprocessing object:
-def load_object(filepath:Path):
+def load_object(filepath: Path):
     try:
         filepath = Path(filepath)
-        with open(filepath) as file_obj:
+        with open(filepath, "rb") as file_obj:
             return pickle.load(file_obj)
         file_obj.close()
     except Exception as ex:
@@ -34,21 +33,26 @@ def load_object(filepath:Path):
 
 
 # numeric and categoric columns:
-def numeric_categoric_columns(raw_data_path):
+def numeric_categoric_columns(raw_data_path: Path) -> Tuple[List[str], List[str]]:
     # reading raw data:
     raw_df = pd.read_csv(raw_data_path)
-    raw_df.drop('price', axis=1, inplace=True)
+    raw_df.drop("price", axis=1, inplace=True)
     # numeric and categoric columns:
     numeric_col = []
     categoric_col = []
     for col in raw_df.columns:
-        if raw_df[col].dtype=='object':
+        if raw_df[col].dtype == "object":
             categoric_col.append(col)
         else:
             numeric_col.append(col)
     # returning numeric and categoric columns:
-    return(
-        numeric_col,
-        categoric_col
-    )
+    return (numeric_col, categoric_col)
 
+
+# categoric columns order:
+def categoric_col_order() -> Tuple[List[str], List[str], List[str]]:
+    cut_order = ["Fair", "Good", "Very Good", "Premium", "Ideal"]
+    color_order = ["J", "I", "H", "G", "F", "E", "D"]
+    clarity_order = ["I1", "SI2", "SI1", "VS2", "VS1", "VVS2", "VVS1", "IF"]
+
+    return (cut_order, color_order, clarity_order)
