3488412a8ebc4c522a672f09b1c306443d7c74b0
yuvaneshkm <yuvaneshkm05@gmail.com>
2024-07-20 22:00:47 +0530

    model-evaluation

--- src/components/model_evaluation.py
+++ src/components/model_evaluation.py
@@ -15,8 +15,16 @@ class ModelEvaluation:
     def __init__(self):
         pass
 
-    def evaluate_metrics(self):
-        pass
+    def evaluate_metrics(self, y_test, y_pred):
+
+        # mean squared error:
+        mse = mean_squared_error(y_test, y_pred)
+        # mean absolute error:
+        mae = mean_absolute_error(y_test, y_pred)
+        # r square value (accuracy):
+        r2 = r2_score(y_test, y_pred)
+
+        return (mse, mae, r2)
 
     def initiate_model_evaluation(self):
         pass
