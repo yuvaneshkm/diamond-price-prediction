e5ced6f2b911ab73c2ae26a434a39a9ec98600b9
yuvaneshkm <yuvaneshkm05@gmail.com>
2024-07-22 03:15:36 +0530

    model-evaluation-updated

--- src/components/model_evaluation.py
+++ src/components/model_evaluation.py
@@ -1,6 +1,7 @@
 # Importing necessary libraries:
 import os
 from pathlib import Path
+from urllib.parse import urlparse
 from typing import Tuple
 import pandas as pd
 import mlflow
@@ -19,19 +20,18 @@ class ModelEvaluation:
     def evaluate_metrics(self, y_test, y_pred) -> Tuple[float, float, float]:
         """The Output of the method is
         (mean_squared_error, mean_absolute_error, r2_score)"""
-
         try:
             # mean squared error:
-            logging.info("MSE calculated")
             MSE = mean_squared_error(y_test, y_pred)
+            logging.info("MSE calculated")
 
             # mean absolute error:
-            logging.info("MAE calculated")
             MAE = mean_absolute_error(y_test, y_pred)
+            logging.info("MAE calculated")
 
             # r square value (accuracy):
-            logging.info("R2_Score calculated")
             R2_Score = r2_score(y_test, y_pred)
+            logging.info("R2_Score calculated")
 
         except Exception as ex:
             raise CustomException(ex)
@@ -57,19 +57,20 @@ class ModelEvaluation:
             # parameters of the model:
             model_params = model.get_params()
 
-            # set the registry uri:
-            mlflow.set_registry_uri("")
+            # prediction of test data (X_test):
+            logging.info("Predicting the test data (X_test values)")
+            y_pred = model.predict(X_test)
+
+            # calculating mse mae and r2_score:
+            logging.info("Calculating MAE, MSE and R2_Score")
+            (mse, mae, r2) = self.evaluate_metrics(y_test, y_pred)
+
+            # set the remote server uri for tracking and model registry:
+            # remote_server_uri = ""
+            # mlflow.set_tracking_uri(remote_server_uri)
 
             # start the mlflow run:
             with mlflow.start_run():
-
-                # prediction of test data (X_test):
-                logging.info("Predicting the test data (X_test values)")
-                y_pred = model.predict(X_test)
-
-                # calculating mse mae and r2_score:
-                logging.info("Calculating MAE, MSE and R2_Score")
-                (mse, mae, r2) = self.evaluate_metrics(y_test, y_pred)
 
                 # logging the metrice:
                 logging.info("Logging Metrics to MLflow")
@@ -79,6 +80,18 @@ class ModelEvaluation:
                 logging.info("Logging Model Parameters to MLflow")
                 mlflow.log_params(model_params)
 
-            pass
+                # type of the tracking uri:
+                tracking_uri_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+                # model registry doesnot work with file source:
+                if tracking_uri_type_store != "file":
+                    mlflow.sklearn.log_model(
+                        model, "model", registered_model_name="diamond-price-predictor"
+                    )
+                    logging.info("Registered the model")
+                else:
+                    mlflow.sklearn.log_model(model, "model")
+                    logging.info("Logged the model")
+
         except Exception as ex:
             raise CustomException(ex)
