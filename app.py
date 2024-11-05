c11d4d9f30f6d386ef34675797182b40f32d7acc
yuvaneshkm <yuvaneshkm05@gmail.com>
2024-11-05 10:00:59 +0530

    ui-created

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 # importing necessary libraries:
 from flask import Flask, render_template, request
+import pandas as pd
+
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -15,6 +17,7 @@ def index():
 @app.route("/result", methods=["GET","POST"])
 def result():
     if request.method=="POST":
+
         carat = request.form["carat"]
         cut = request.form["cut"]
         color = request.form["color"]
@@ -24,6 +27,20 @@ def result():
         x = request.form["x"]
         y = request.form["y"]
         z = request.form["z"]
+
+        prediction_data_dict = {
+            "carat" : [float(carat)],
+            "cut" : [str(cut)],
+            "color" : [str(color)],
+            "clarity" : [str(clarity)],
+            "depth" : [float(depth)],
+            "table" : [float(table)],
+            "x" : [float(x)],
+            "y" : [float(y)],
+            "z" : [float(z)]
+        }
+
+        prediction_data = pd.DataFrame(prediction_data_dict)
 
     return render_template("result.html")
 
